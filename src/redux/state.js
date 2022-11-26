d144293ca5908cda99509c61d68a456a73fcd226
Elena Christensen <olenachristensen777@gmail.com>
2022-11-26 10:43:32 +0100

    portfolio

--- src/redux/state.js
+++ src/redux/state.js
@@ -10,7 +10,7 @@ let state = {
     neoTradition: "neoTradition ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 
-  activeStyle: 'fineLine',
+  activeStyle: 'neoTradition',
 
   
   gallery: {
@@ -35,7 +35,7 @@ let state = {
       '1.jpg',
     ],
 
-    neoTraditional: [
+    neoTradition: [
       '1.jpg',
       '2.jpg',
       '3.jpg',
